4712da83ac9c5a900cbdd961109c149a29102e1f
MrkWinter <744988414@qq.com>
2023-10-15 11:00:47 +0800

    后端学习

---
+++ backEnd/_1_maven_test/maven-project01/src/main/java/com/itheima/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.itheima;
+
+/**
+ * @author MrkWinter
+ * @version 1.0
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+    public void cry() {
+        System.out.println("哇");
+    }
+}
---
+++ backEnd/_1_maven_test/maven-project01/src/test/java/com/itheima/HelloWorldTest.java
@@ -0,0 +1,18 @@
+package com.itheima;
+
+import org.junit.Test;
+
+/**
+ * @author MrkWinter
+ * @version 1.0
+ */
+public class HelloWorldTest {
+    public static void main(String[] args) {
+
+    }
+
+    @Test
+    public void cry() {
+        System.out.println("哇");
+    }
+}
